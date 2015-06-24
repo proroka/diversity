c5461e62bba0f58dd04e59c8d8a6c53ff9eb4cc0
proroka <amanda.prorok@gmail.com>
2015-06-24 18:02:37 -0400

    ..

---
+++ stochastic/micro.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 24 17:43:50 2015
+
+@author: amandaprorok
+"""
+
+
+import numpy as np
+import pylab as pl
+import matplotlib.pyplot as pp
+
+# my modules
