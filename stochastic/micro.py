5bd92cda570d9bb4a82a24c1b6557a15af6601ab
proroka <amanda.prorok@gmail.com>
2015-06-26 19:20:20 -0400

    added some functionalities to micro

--- stochastic/micro.py
+++ stochastic/micro.py
@@ -7,6 +7,7 @@ Created on Wed Jun 24 17:43:50 2015
 
 
 import numpy as np
+import scipy as sp
 import pylab as pl
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -26,12 +27,13 @@ import funcdef_micro as fd
 # -----------------------------------------------------------------------------#
 # initialize
 # create 2d lattice graph
-size_lattice = 3
+size_lattice = 2
 num_nodes = size_lattice**2
 graph = nx.grid_2d_graph(size_lattice, size_lattice) #, periodic = True)
 # get the adjencency matrix
 adjacency_m = nx.to_numpy_matrix(graph)
 adjacency_m = np.squeeze(np.asarray(adjacency_m))
+
 
 # initialize robot population, random node allocation
 num_robots = 10
@@ -43,11 +45,6 @@ for i in range(num_nodes):
     deploy_robots_init[i] = np.size(np.where(state_robots_init == i))
 deploy_robots_init /= np.sum(deploy_robots_init)
 
-# draw graph with node size proportional to robot population
-scale = 1000 # scale size of node
-print 'Initial configuration:'
-nx.draw_circular(graph, node_size=deploy_robots_init*scale)
-plt.show()
 
 # create random transition matrix
 transition_m_init = np.random.rand(num_nodes, num_nodes) * adjacency_m
@@ -55,8 +52,14 @@ transition_m = transition_m_init.copy()
 # k_ii is -sum(k_ij) s.t. sum(column)=0; ensures constant total number of robots
 np.fill_diagonal(transition_m, -np.sum(transition_m_init,0)) 
 
+# for testing
+#deploy_robots_init = np.array([1., 0., 0., 0.])
+#deploy_robots_desired = np.array([0., 0., 0., 1.])
+
+
 # -----------------------------------------------------------------------------#
-# run macroscopic evolution of dynamical system
+# run euler integration 
+
 t_max = 100
 delta_t = 0.1
 deploy_robots = np.zeros((num_nodes,t_max))
@@ -64,31 +67,51 @@ for i in range(num_nodes):
     deploy_robots[i,0] = deploy_robots_init[i]
 for t in range(1,t_max):
     deploy_robots[:,t] = deploy_robots[:,t-1] + delta_t*np.dot(transition_m, deploy_robots[:,t-1])
-        
+       
+
+# draw graph with node size proportional to robot population
+scale = 1000 # scale size of node
+print 'Initial configuration:'
+nx.draw_circular(graph, node_size=deploy_robots_init*scale)
+plt.show()       
 # draw graph with node size proportional to robot population
 print 'Final configuration:'
 nx.draw_circular(graph, node_size=deploy_robots[:,t_max-1]*scale)
 plt.show()
 
-# analytic version
-A = transition_m.copy()
-x0 = deploy_robots_init.copy()
-x_tmax = np.dot(np.exp(A*t_max), x0)
-
-diff = x_tmax - deploy_robots[:,t_max-1]
-print diff
 
 # plot evolution of robot population over time
 for n in range(num_nodes):
-    x = np.arange(0,t_max)
+    x = np.arange(0, t_max)
     y = deploy_robots[n,:]
     plt.plot(x,y)
     
 plt.show()
 
 
+# -----------------------------------------------------------------------------#
+# solve system analytically
+
+eig_values, eig_vectors = np.linalg.eig(transition_m)
+i = np.argmax(eig_values) 
+steady_state = eig_vectors[:,i].astype(float) 
+
+steady_state /= sum(steady_state)  # Makes sure that the sum of all robots is 1.
+diff = steady_state.T - deploy_robots[:,t_max-1]
+
+print 'Final divergence (analytical vs euler): \n', diff
 
 
+# -----------------------------------------------------------------------------#
+# solve system analytically
+
+# analytic version
+A = transition_m.copy()
+x0 = deploy_robots_init.copy()
+x_tmax = np.dot(sp.linalg.expm(A*t_max), x0)
+diff2 = x_tmax.T - deploy_robots[:,t_max-1]
+
+print 'Final divergence (exponential vs euler): \n', diff2
 
 
 
