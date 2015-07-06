added new files (adaptive micro with receding horizon; gradient of cost function; also fixed some issues in funcdef files.

stochastic/funcdef_micro_heterogeneous.py
@@ -92,25 +92,7 @@ def microscopic_sim(t_max, dt, robots_init, deploy_robots_init, transition_m):
 # -----------------------------------------------------------------------------#
 # run euler integration and return time evolution
 
-"""
 def run_euler_integration_micro(t_max, delta_t, deploy_robots_init, transition_m):
-    
-    num_nodes = deploy_robots_init.shape[0]
-    num_species = deploy_robots_init.shape[1]
-    
-
-    deploy_robots = np.zeros((num_nodes,t_max, num_species))
-    for s in range(num_species):
-        for i in range(num_nodes):  
-            deploy_robots[i,0,s] = deploy_robots_init[i,s]
-            for t in range(1,t_max):
-                deploy_robots[:,t,s] = deploy_robots[:,t-1,s] + delta_t*np.dot(transition_m[:,:,s], deploy_robots[:,t-1,s]) 
-    
-    return deploy_robots
-    
-   """
-
- def run_euler_integration_micro(t_max, delta_t, deploy_robots_init, transition_m):
 
     num_nodes = deploy_robots_init.shape[0]
     num_species = deploy_robots_init.shape[1]
@@ -123,3 +105,6 @@ def run_euler_integration_micro(t_max, delta_t, deploy_robots_init, transition_m
             deploy_robots[:,t,s] = deploy_robots[:,t-1,s] + delta_t*np.dot(transition_m[:,:,s], deploy_robots[:,t-1,s])
 
     return deploy_robots   
+    
+    
+    

stochastic/micro_hetero_adaptive.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jun 29 10:53:24 2015
+Created on Sun Jul  5 16:40:07 2015
 @author: amandaprorok
 
 """
+
 
 import numpy as np
 import scipy as sp
@@ -14,29 +15,29 @@ import networkx as nx
 # my modules
 from optimize_transition_matrix_hetero import *
 from funcdef_macro_heterogeneous import *
+from funcdef_micro_heterogeneous import *
 
 # -----------------------------------------------------------------------------#
 
 # 1. create graph
 # 2. initialize robot deployment on nodes
 # 3. initialize transition matrix
-# 4. run macro-discrete model 
-# 5. plot evolution of robot population per node on graph
-
+# 4. run macro-discrete model  / micro simulator
+# 5. plot evolution of robot population per node 
 
 # -----------------------------------------------------------------------------#
 # initialize world and robot community
 
 # create network of sites
-size_lattice = 3
+size_lattice = 2
 num_nodes = size_lattice**2
 
 # set of traits
-num_traits = 3
+num_traits = 2
 max_trait_values = 2 # [0,1]: trait availability
 
 # robot species
-num_species = 3
+num_species = 2
 max_robots = 10 # maximum number of robots per node
 deploy_robots_init = np.random.randint(0, max_robots, size=(num_nodes, num_species))
 
@@ -50,14 +51,16 @@ random_transition = random_transition_matrix(num_nodes)
 deploy_robots_final = sample_final_robot_distribution(deploy_robots_init, random_transition)
 deploy_traits_desired = np.dot(deploy_robots_final, species_traits)
 
+# initialize robots
+robots = initialize_robots(deploy_robots_init)
+     
+    
 # -----------------------------------------------------------------------------#
-
 
 print "total robots, init: \t", np.sum(np.sum(deploy_robots_init))
 print "total robots, final: \t", np.sum(np.sum(deploy_robots_final))
 print "total traits, init: \t", np.sum(np.sum(deploy_traits_init))
 print "total traits, desired:\t", np.sum(np.sum(deploy_traits_desired))
-
 
 # -----------------------------------------------------------------------------#
 # initialize graph: all species move on same graph
@@ -71,31 +74,67 @@ adjacency_m = np.squeeze(np.asarray(adjacency_m))
 # -----------------------------------------------------------------------------#
 # find optimal transition matrix
 
-transition_m = optimal_transition_matrix(adjacency_m, deploy_robots_init, deploy_traits_desired, species_traits)
-
+transition_m_init = optimal_transition_matrix(adjacency_m, deploy_robots_init, deploy_traits_desired, species_traits)
       
 # -----------------------------------------------------------------------------#
-# run euler integration to drive robots to end state
+# run microscopic stochastic simulation
 
-deploy_robots = run_euler_integration(deploy_robots_init, transition_m)
+t_max = 90
+dt = 0.1
+num_iter = 2
 
-deploy_robots_final = deploy_robots[:,-1,:]
-deploy_traits_final = np.dot(deploy_robots_final, species_traits) 
+deploy_robots_micro = np.zeros((num_nodes, t_max, num_species, num_iter)) 
+for it in range(num_iter): 
+    print "Iteration: ", it
+    robots_new, deploy_robots_micro[:,:,:,it] = microscopic_sim(t_max, dt, robots, deploy_robots_init, transition_m_init)
+avg_deploy_robots_micro = np.mean(deploy_robots_micro,3)
+   
+# -----------------------------------------------------------------------------#
+# run adaptive microscopic stochastic simulation
 
+
+slices = 3
+t_window = int(t_max / slices)
+
+deploy_robots_micro_adapt = np.zeros((num_nodes, t_max, num_species, num_iter)) 
+
+
+for it in range(num_iter): 
+    transition_m = transition_m_init.copy()
+    deploy_robots_init_slice = deploy_robots_init.copy()
+    robots_slice = robots.copy()
+    for sl in range(slices):
+        print "Slice: ", sl
+        robots_slice, deploy_robots_micro_slice = microscopic_sim(t_window, dt, robots_slice, deploy_robots_init_slice, transition_m)
+        deploy_robots_init_slice = deploy_robots_micro_slice[:,-1,:]
+        # put together slices
+        deploy_robots_micro_adapt[:,sl*t_window:(sl+1)*t_window,:,it] = deploy_robots_micro_slice
+        # calculate adapted transition matrix    
+        transition_m = optimal_transition_matrix(adjacency_m, deploy_robots_init_slice, deploy_traits_desired, species_traits)
+
+avg_deploy_robots_micro_adapt = np.mean(deploy_robots_micro_adapt,3)
+
+# -----------------------------------------------------------------------------#
+# euler integration
+       
+deploy_robots_euler = run_euler_integration_micro(t_max, dt, deploy_robots_init, transition_m_init)
 
 # -----------------------------------------------------------------------------#
 # plots
 
-plot_network(graph, deploy_traits_init, deploy_traits_final)
-# plot_histogram(deploy_traits_final)
 
 # plot evolution over time
 species_index = 0
 trait_index = 0
-plot_robots_time(deploy_robots, species_index)
-plot_traits_time(deploy_robots, species_traits, trait_index)
-    
-   
+
+plot_robots_time(deploy_robots_euler, species_index)
+plot_robots_time(avg_deploy_robots_micro, species_index)
+plot_robots_time(avg_deploy_robots_micro_adapt, species_index)
+
+
+#plot_traits_time(deploy_robots, species_traits, trait_index)
+ 
+ 
 
 
 