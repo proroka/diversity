cde8a6f0aa4d51b9e6f898ecf432e24d3dd1bb2e
proroka <amanda.prorok@gmail.com>
2015-08-07 15:34:17 -0400

    small changes

--- stochastic/generate_Q.py
+++ stochastic/generate_Q.py
@@ -19,9 +19,6 @@ import pickle
 
 
 
-
-
-
 # -----------------------------------------------------------------------------#
 def generate_Q(S, U):
     
@@ -69,20 +66,27 @@ def generate_Q(S, U):
     
 # -----------------------------------------------------------------------------#
 
-num_species = 5
-num_traits = 5
+num = 5
+
+# -----------------------------------------------------------------------------#
+
+num_species = 10
+num_traits = 10
 q_rs = np.zeros((num_species, num_traits, num))
 q_rs1 = np.zeros((num_species, num_traits, num))
-
+q_rs2 = np.zeros((num_species, num_traits, num))
+run = 'S10_U10'
 
 max_rk = np.min([num_species, num_traits])
-num = 5
+
 
 not_full = True
 nrs = 0
 nrs1 = 0
+nrs2 = 0
 cnt_rs = 0
 cnt_rs1 = 0
+cnt_rs2 = 0
 while not_full:
     Q, rk, s = generate_Q(num_species, num_traits)
     if rk==max_rk:
@@ -97,14 +101,114 @@ while not_full:
         if nrs1<num-1:        
             nrs1 +=1
         cnt_rs1 += 1    
-    if (cnt_rs>=num and cnt_rs1>=num):
+    if rk==(max_rk-2):
+        q_rs2[:,:,nrs2] = Q
+        print 'Found rk=S-2, num = ', nrs2
+        if nrs2<num-1:        
+            nrs2 +=1
+        cnt_rs2 += 1    
+    if (cnt_rs>=num and cnt_rs1>=num and cnt_rs2>=num):
         not_full = False
 
+
+prefix = "./data/Q/" + run + "_"
+
+pickle.dump(q_rs, open(prefix+"q_rs.p", "wb"))
+pickle.dump(q_rs1, open(prefix+"q_rs1.p", "wb"))
+pickle.dump(q_rs2, open(prefix+"q_rs2.p", "wb"))
+
 # -----------------------------------------------------------------------------#
 
-num_species = 8
-num_traits = 5
+num_species = 10
+num_traits = 15
+q_rs = np.zeros((num_species, num_traits, num))
+q_rs1 = np.zeros((num_species, num_traits, num))
+q_rs2 = np.zeros((num_species, num_traits, num))
+run = 'S10_U15'
+
+max_rk = np.min([num_species, num_traits])
+
+not_full = True
+nrs = 0
+nrs1 = 0
+nrs2 = 0
+cnt_rs = 0
+cnt_rs1 = 0
+cnt_rs2 = 0
+while not_full:
+    Q, rk, s = generate_Q(num_species, num_traits)
+    if rk==max_rk:
+        q_rs[:,:,nrs] = Q
+        print 'Found rk=S, num = ', nrs
+        if nrs<num-1:
+            nrs += 1
+        cnt_rs += 1            
+    if rk==(max_rk-1):
+        q_rs1[:,:,nrs1] = Q
+        print 'Found rk=S-1, num = ', nrs1
+        if nrs1<num-1:        
+            nrs1 +=1
+        cnt_rs1 += 1    
+    if rk==(max_rk-2):
+        q_rs2[:,:,nrs2] = Q
+        print 'Found rk=S-2, num = ', nrs2
+        if nrs2<num-1:        
+            nrs2 +=1
+        cnt_rs2 += 1    
+    if (cnt_rs>=num and cnt_rs1>=num and cnt_rs2>=num):
+        not_full = False
+
+
+prefix = "./data/Q/" + run + "_"
+
+pickle.dump(q_rs, open(prefix+"q_rs.p", "wb"))
+pickle.dump(q_rs1, open(prefix+"q_rs1.p", "wb"))
+pickle.dump(q_rs2, open(prefix+"q_rs2.p", "wb"))
 
 # -----------------------------------------------------------------------------#
-num_species = 5
-num_traits = 8
+
+num_species = 15
+num_traits = 10
+q_rs = np.zeros((num_species, num_traits, num))
+q_rs1 = np.zeros((num_species, num_traits, num))
+q_rs2 = np.zeros((num_species, num_traits, num))
+run = 'S15_U10'
+
+max_rk = np.min([num_species, num_traits])
+
+not_full = True
+nrs = 0
+nrs1 = 0
+nrs2 = 0
+cnt_rs = 0
+cnt_rs1 = 0
+cnt_rs2 = 0
+while not_full:
+    Q, rk, s = generate_Q(num_species, num_traits)
+    if rk==max_rk:
+        q_rs[:,:,nrs] = Q
+        print 'Found rk=S, num = ', nrs
+        if nrs<num-1:
+            nrs += 1
+        cnt_rs += 1            
+    if rk==(max_rk-1):
+        q_rs1[:,:,nrs1] = Q
+        print 'Found rk=S-1, num = ', nrs1
+        if nrs1<num-1:        
+            nrs1 +=1
+        cnt_rs1 += 1    
+    if rk==(max_rk-2):
+        q_rs2[:,:,nrs2] = Q
+        print 'Found rk=S-2, num = ', nrs2
+        if nrs2<num-1:        
+            nrs2 +=1
+        cnt_rs2 += 1    
+    if (cnt_rs>=num and cnt_rs1>=num and cnt_rs2>=num):
+        not_full = False
+
+
+prefix = "./data/Q/" + run + "_"
+
+pickle.dump(q_rs, open(prefix+"q_rs.p", "wb"))
+pickle.dump(q_rs1, open(prefix+"q_rs1.p", "wb"))
+pickle.dump(q_rs2, open(prefix+"q_rs2.p", "wb"))
