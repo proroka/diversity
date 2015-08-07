2557dcae2ea28611d2097c5242a4c68ba84aea22
proroka <amanda.prorok@gmail.com>
2015-08-07 15:18:15 -0400

    small changes

--- stochastic/generate_Q.py
+++ stochastic/generate_Q.py
@@ -24,6 +24,9 @@ import pickle
 
 # -----------------------------------------------------------------------------#
 def generate_Q(S, U):
+    
+    verbose = 0
+    
     # fill all rows and columns with at least one 1
     if S==U:
         Q = np.identity(S) 
@@ -42,7 +45,7 @@ def generate_Q(S, U):
 
  
     num = np.random.randint(0,U*S+1)    
-    num = 0
+    #num = 0
     # fill matrix    
     for n in range(num):
         i = np.random.randint(0,S)
@@ -50,21 +53,58 @@ def generate_Q(S, U):
         Q[i,j] = 1
         
     rk = np.linalg.matrix_rank(Q)    
-       
+    s = int(np.sum(np.sum(Q)))
     
-    print Q
-    #print 'Q with S =', S, 'has rank ', rk , 'with ', num, 'extra 1s'
-    print 'S = ', S
-    print 'R = ', rk    
-    plt.imshow(Q, interpolation='none')
-    plt.show()
+    if verbose:
+        print Q
+        print 'S = ', S
+        print 'U = ', U
+        print 'R = ', rk    
+        print '1 = ', s
+        plt.imshow(Q, interpolation='none')
+        plt.show()
+        
     
-    return Q, rk
+    return Q, rk, s
     
 # -----------------------------------------------------------------------------#
 
 num_species = 5
-num_traits = 10
+num_traits = 5
+q_rs = np.zeros((num_species, num_traits, num))
+q_rs1 = np.zeros((num_species, num_traits, num))
 
-generate_Q(num_species, num_traits)
 
+max_rk = np.min([num_species, num_traits])
+num = 5
+
+not_full = True
+nrs = 0
+nrs1 = 0
+cnt_rs = 0
+cnt_rs1 = 0
+while not_full:
+    Q, rk, s = generate_Q(num_species, num_traits)
+    if rk==max_rk:
+        q_rs[:,:,nrs] = Q
+        print 'Found rk=S, num = ', nrs
+        if nrs<num-1:
+            nrs += 1
+        cnt_rs += 1            
+    if rk==(max_rk-1):
+        q_rs1[:,:,nrs1] = Q
+        print 'Found rk=S-1, num = ', nrs1
+        if nrs1<num-1:        
+            nrs1 +=1
+        cnt_rs1 += 1    
+    if (cnt_rs>=num and cnt_rs1>=num):
+        not_full = False
+
+# -----------------------------------------------------------------------------#
+
+num_species = 8
+num_traits = 5
+
+# -----------------------------------------------------------------------------#
+num_species = 5
+num_traits = 8
