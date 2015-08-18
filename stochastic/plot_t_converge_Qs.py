7bf688ed0988899081f9d9090da341f2bc35a07c
proroka <amanda.prorok@gmail.com>
2015-08-18 11:22:59 -0400

    shaded plotting function

--- stochastic/plot_t_converge_Qs.py
+++ stochastic/plot_t_converge_Qs.py
@@ -24,8 +24,10 @@ from funcdef_util_heterogeneous import *
 import funcdef_draw_network as nxmod
 
 
-run = 'Q7'
-berman = False
+# good runs for rank: Q5
+# good runs for ORrank:
+run = 'Q5'
+berman = True
 
 # -----------------------------------------------------------------------------#
 def plot_t_converge(delta_t, t_min_a, t_min_b):
@@ -80,7 +82,83 @@ def plot_t_converge(delta_t, t_min_a, t_min_b):
 
     return fig
     
+# -----------------------------------------------------------------------------#
+def plot_t_converge_shaded(delta_t, t_min_a, t_min_b=None):
 
+    num_rk = t_min_a.shape[1]
+    macro = (np.size(t_min_a.shape)==2)
+    
+    if not macro:
+        el = t_min_a.shape[0] * t_min_a.shape[2]    
+    else:
+        el = t_min_a.shape[0]
+        
+    ta_f = np.zeros((num_rk, el))
+    tb_f = np.zeros((num_rk, el))
+    pc_a = np.zeros((num_rk,3))
+    pc_b = np.zeros((num_rk,3))
+    p0 = 25
+    p1 = 50
+    p2 = 75
+    labels = []
+    
+    if t_min_b is None:
+        for rk in range(num_rk):
+            if not macro:
+                ta_f[rk,:] = delta_t * t_min_a[:,rk,:].flatten()
+            else:
+                ta_f[rk,:] = delta_t * t_min_a[:,rk].flatten()
+            pc_a[rk,0] = np.percentile(ta_f[rk,:],p0)
+            pc_a[rk,1] = np.percentile(ta_f[rk,:],p1)
+            pc_a[rk,2] = np.percentile(ta_f[rk,:],p2)
+            labels.append(str(rk+1))
+    else:
+        for rk in range(num_rk):
+            if not macro:
+                ta_f[rk,:] = delta_t * t_min_a[:,rk,:].flatten()
+                tb_f[rk,:] = delta_t * t_min_b[:,rk,:].flatten()
+            else:
+                ta_f[rk,:] = delta_t * t_min_a[:,rk].flatten()
+                tb_f[rk,:] = delta_t * t_min_b[:,rk].flatten()
+            pc_a[rk,0] = np.percentile(ta_f[rk,:],p0)
+            pc_a[rk,1] = np.percentile(ta_f[rk,:],p1)
+            pc_a[rk,2] = np.percentile(ta_f[rk,:],p2)
+            pc_b[rk,0] = np.percentile(tb_f[rk,:],p0)
+            pc_b[rk,1] = np.percentile(tb_f[rk,:],p1)
+            pc_b[rk,2] = np.percentile(tb_f[rk,:],p2)
+            labels.append(str(rk+1))
+    
+    N = num_rk*2 + 2
+    fig = plt.figure()
+    ax = plt.gca()
+
+    x = range(1,num_rk+1)
+    x_pos = []
+    for i in range(num_rk):
+        x_pos.append(x[i]-0.15)
+        x_pos.append(x[i]+0.15)
+          
+    
+    
+    l1 = plt.plot(x, pc_a[:,1], color='green', linewidth=2, label='explicit')
+    plt.fill_between(x, pc_a[:,2], pc_a[:,0], facecolor='green', alpha=0.3)
+    if t_min_b is not None:    
+        l2 = plt.plot(x, pc_b[:,1], color='red', linewidth=2, label='implicit')
+        plt.fill_between(x, pc_b[:,2], pc_b[:,0], facecolor='red', alpha=0.3)
+        
+    plt.legend(loc='upper right', shadow=False, fontsize='large')  
+    
+    plt.grid(axis='y')
+    ymin = 0 
+    ymax = 6
+    ax.set_ylim([0, ymax])    
+    ax.set_xlim([1, num_rk])
+
+
+    plt.xticks(x, labels) #, rotation='vertical')
+
+    return fig
+    
     
 # -----------------------------------------------------------------------------#
 # load data
@@ -100,9 +178,9 @@ if berman:
 delta_t = 0.04
 
 if berman:
-    fig1 = plot_t_converge(delta_t, t_min_mic, t_min_mic_ber)
+    fig1 = plot_t_converge_shaded(delta_t, t_min_mic, t_min_mic_ber)
 else:
-    fig1 = plot_t_converge(delta_t, t_min_mic, t_min_mic)
+    fig1 = plot_t_converge_shaded(delta_t, t_min_mic)
 plt.axes().set_aspect(0.5,'box')
 plt.show()
 
