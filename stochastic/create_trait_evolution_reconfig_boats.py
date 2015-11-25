1d45169079af97193e5b98c22a0727feb43c4894
proroka <amanda.prorok@gmail.com>
2015-11-24 19:21:58 -0500

    added boats optimization for matlab

--- stochastic/create_trait_evolution_reconfig_boats.py
+++ stochastic/create_trait_evolution_reconfig_boats.py
@@ -24,9 +24,9 @@ import funcdef_draw_network as nxmod
 
 # -----------------------------------------------------------------------------#
 # initialize world and robot community
-run = 'B1'
+run = 'B01'
 
-save_data = 0
+save_data = 1
 
 
 save_globals = False
@@ -37,31 +37,36 @@ fix_final = True
 
 
 # simulation parameters
-t_max = 10.0 # influences desired state and optmization of transition matrix
-t_max_sim = 8.0 # influences simulations and plotting
+t_max = 8.0 # influences desired state and optmization of transition matrix
+t_max_sim = 7.0 # influences simulations and plotting
 delta_t = 0.04 # time step
-max_rate = 2.0 # Maximum rate possible for K.
+max_rate = 1.0 # Maximum rate possible for K.
 
 # cost function
 l_norm = 2 # 2: quadratic 1: absolute
 match = 1 # 1: exact 0: at-least
-
-
+FSS = 4.0
 
 # create network of sites
-num_nodes = 8
-# set of traits
-num_traits = 4
-max_trait_values = 2 # [0,1]: trait availability
+num_nodes = 10
+
 # robot species
-num_species = 3
-max_robots = 300 # maximum number of robots per node
-deploy_robots_init = np.random.randint(0, max_robots, size=(num_nodes, num_species))
-
-
+total_num_robots = 100.0 
 num_species = 3
 num_traits = 4
+
 species_traits = np.array([[1,0,1,0],[1,0,0,1],[0,1,0,1]])
+sum_species = np.array([30,10,60])
+
+deploy_robots_init = np.random.randint(0, 100, size=(num_nodes, num_species))
+
+# normalize initial distribution
+ts = np.sum(deploy_robots_init, axis=0)
+for i in range(num_species):
+    deploy_robots_init[:,i] = deploy_robots_init[:,i] / float(ts[i]) * float(sum_species[i])
+
+sum_species_t = np.sum(deploy_robots_init,axis=0)
+print 'sum_species_t', sum_species_t
 
 # -----------------------------------------------------------------------------#
 # initialize graph: all species move on same graph
@@ -83,23 +88,18 @@ deploy_robots_final_0 = deploy_robots_final.copy()
 deploy_robots_final_1 = deploy_robots_final.copy()
 deploy_robots_final_2 = deploy_robots_final.copy()
 
-#deploy_robots_final_0[:2,:] = 2
-#deploy_robots_final_1[2:4,:] = 2
-#deploy_robots_final_2[4:,:] = 2
 
-deploy_robots_final_0 = np.random.randint(0, max_robots, size=(num_nodes, num_species)).astype(float)
-deploy_robots_final_1 = np.random.randint(0, max_robots, size=(num_nodes, num_species)).astype(float)
-deploy_robots_final_2 = np.random.randint(0, max_robots, size=(num_nodes, num_species)).astype(float)
+
+deploy_robots_final_0 = np.random.randint(0, 100, size=(num_nodes, num_species)).astype(float)
+deploy_robots_final_1 = np.random.randint(0, 100, size=(num_nodes, num_species)).astype(float)
+deploy_robots_final_2 = np.random.randint(0, 100, size=(num_nodes, num_species)).astype(float)
 
 
 # fix initial
 deploy_robots_init_0 = deploy_robots_init.copy()
-deploy_robots_init_0[3:,:] = 0
+#deploy_robots_init_0[3:,:] = 0
 
-
-sum_species = np.sum(deploy_robots_init_0,axis=0)
-
-
+# normalize final distribution
 ts0 = np.sum(deploy_robots_final_0, axis=0)
 ts1 = np.sum(deploy_robots_final_1, axis=0)
 ts2 = np.sum(deploy_robots_final_2, axis=0)
@@ -107,11 +107,11 @@ for i in range(num_species):
     deploy_robots_final_0[:,i] = deploy_robots_final_0[:,i] / float(ts0[i]) * float(sum_species[i])
     deploy_robots_final_1[:,i] = deploy_robots_final_1[:,i] / float(ts1[i]) * float(sum_species[i])
     deploy_robots_final_2[:,i] = deploy_robots_final_2[:,i] / float(ts2[i]) * float(sum_species[i])
-print np.sum(deploy_robots_final_0)
-print np.sum(deploy_robots_final_1)
-print np.sum(deploy_robots_final_2)
+print 'final_0', np.sum(deploy_robots_final_0, axis=0)
+print 'final_1', np.sum(deploy_robots_final_1, axis=0)
+print 'final_2', np.sum(deploy_robots_final_2, axis=0)
 
-# allocate the following intial distribs
+# allocate the following intial distribs as previous final
 deploy_robots_init_1 = deploy_robots_final_0.copy()
 deploy_robots_init_2 = deploy_robots_final_1.copy()
 
@@ -129,7 +129,7 @@ init_transition_values = np.array([])
 print "****"
 print "Optimizing 1st transition matrix"
 transition_m_0 = optimal_transition_matrix(init_transition_values, adjacency_m, deploy_robots_init_0, deploy_traits_desired_0,
-                                           species_traits, t_max, max_rate, l_norm, match, optimizing_t=True, force_steady_state=4.0)
+                                           species_traits, t_max, max_rate, l_norm, match, optimizing_t=True, force_steady_state=FSS)
 print "****"
 print "Running Euler for 1st transition matrix"
 deploy_robots_euler_0 = run_euler_integration(deploy_robots_init_0, transition_m_0, t_max_sim, delta_t)
@@ -139,7 +139,7 @@ deploy_traits_init_1 = np.dot(deploy_robots_init_1, species_traits)
 print "****"
 print "Optimizing 2nd transition matrix"
 transition_m_1 = optimal_transition_matrix(init_transition_values, adjacency_m, deploy_robots_init_1, deploy_traits_desired_1,
-                                           species_traits, t_max, max_rate, l_norm, match, optimizing_t=True, force_steady_state=4.0)
+                                           species_traits, t_max, max_rate, l_norm, match, optimizing_t=True, force_steady_state=FSS)
 print "****"
 print "Running Euler for 2nd transition matrix"
 deploy_robots_euler_1 = run_euler_integration(deploy_robots_init_1, transition_m_1, t_max_sim, delta_t)
@@ -149,7 +149,7 @@ deploy_traits_init_2 = np.dot(deploy_robots_init_2, species_traits)
 print "****"
 print "Optimizing 3rd transition matrix"
 transition_m_2 = optimal_transition_matrix(init_transition_values, adjacency_m, deploy_robots_init_2, deploy_traits_desired_2,
-                                           species_traits, t_max, max_rate, l_norm, match, optimizing_t=True, force_steady_state=4.0)
+                                           species_traits, t_max, max_rate, l_norm, match, optimizing_t=True, force_steady_state=FSS)
 print "****"
 print "Running Euler for 3rd transition matrix"
 deploy_robots_euler_2 = run_euler_integration(deploy_robots_init_2, transition_m_2, t_max_sim, delta_t)
@@ -159,14 +159,42 @@ deploy_robots_evolution = np.concatenate((deploy_robots_euler_0, deploy_robots_e
 
 
 # -----------------------------------------------------------------------------#
+# Print transition matrix for matlab
+
+def print_K(Ks, species_traits):
+    print '\nMatlab code to set up the K matrices:'
+    print '-------------------------------------\n'
+    print 'nspecies =', species_traits.shape[0]
+    print 'K = cell(nspecies);'
+    for s in range(species_traits.shape[0]):
+        K = Ks[:, :, s]
+        content = []
+        rows = [K[i] for i in range(K.shape[0])]
+        for row in rows:
+            content.append(' '.join([str(v) for v in row]))
+        print 'K{%d} = [%s];' % (s + 1, '; ...\n        '.join(content))
+    print ''
+
+
+# -----------------------------------------------------------------------------#
+# Print transition matrices for matlab simulation
+
+print_K(transition_m_0, species_traits)
+print_K(transition_m_1, species_traits)
+print_K(transition_m_2, species_traits)
+
+# -----------------------------------------------------------------------------#
 # save variables
 
 if save_data:
 
     tend = time.strftime("%Y%m%d-%H%M%S")
-    prefix = "./data/" + run + "_micmac_"
+    prefix = "./data/" + run + "_evolution_"
     print "Time start: ", tstart
     print "Time end: ", tend
+
+    pickle.dump(sum_species, open(prefix+"ss.p", "wb"))
+    pickle.dump(adjacency_m, open(prefix+"adj.p", "wb"))
 
     pickle.dump(species_traits, open(prefix+"st.p", "wb"))
     pickle.dump(graph, open(prefix+"graph.p", "wb"))
@@ -179,6 +207,8 @@ if save_data:
     pickle.dump(deploy_traits_desired_0, open(prefix+"td_0.p", "wb"))
     pickle.dump(deploy_traits_desired_1, open(prefix+"td_1.p", "wb"))
     pickle.dump(deploy_traits_desired_2, open(prefix+"td_2.p", "wb"))
+    
+    
 
 
 
--- stochastic/plotting/plot_robots_traits_time.py
+++ stochastic/plotting/plot_robots_traits_time.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+o# -*- coding: utf-8 -*-
 """
 Created on Wed Jul 22 09:07:09 2015
 
